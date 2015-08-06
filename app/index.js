7f1c1a36a5293e42e9c297cf3120c40f38b8d4cd
Davis Sylvester <davis@sylvesterllc.com>
2015-08-06 17:27:57 -0500

    Working on Generator

--- app/index.js
+++ app/index.js
@@ -1,13 +1,42 @@
 var generators = require('yeoman-generator');
+var mkdir = require('mkdirp');
 
-module.exports = generators.Base.extend({
-   constructor: function() {
-	generators.Base.apply(this, arguments);
 
-}
+
+
+module.exports = generators.Base.extend({ 	
+
+
+
 	method1: function(){
 
-	console.log('Method1 has run!');
-};
+		console.log('Method1 has run!');
+
+		console.log('Dest Dir: ' + destDir);
+		console.log('Source Dir: ' + srcDir);	
+
+		var destDir = this.destinationRoot();
+		var srcDir = this.sourceRoot();
+
+		var appDir = destDir + '/app/';
+		var sourceDir = destDir + '/src/';
+
+		mkdir(appDir);
+		mkdir(sourceDir);
+
+	},
+
+	makeDirectoryStructure: function(){
+		var destDir = this.destinationRoot();
+		var srcDir = this.sourceRoot();
+
+		var appDir = destDir + '/app/';
+		var sourceDir = destDir + '/src/';
+
+		mkdir(appDir);
+		mkdir(sourceDir);
+	}
+
+
 });
 
